83c297a330bd0636002ae117e3ce90c8895483c6
ascherbinin <ascherbinin@live.ru>
2015-01-29 15:34:21 +0700

    Добавлен скрипт камеры и измененно движение.
    Добавлен скрипт камеры (CameraFollowScript).
    Изменено управление на ввод с клавиатуры (PlayerControl).

---
+++ Assets/FollowCameraScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCameraScript : MonoBehaviour {
+
+	public Transform Target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Target != null) 
+		{
+			Vector3 _targetPos = Target.position;
+			_targetPos.z = transform.position.z;
+			transform.position = _targetPos;
+		}
+	}
+}
--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -6,59 +6,96 @@ public class PlayerControl : MonoBehaviour
 {
 
     //Public Vars
-    public Camera camera;
-    public float speed;
+    
+    public float Speed;
+	public float RotationSpeed;
 
     //Private Vars
-    private Vector3 mousePosition;
-    private Vector3 direction;
-    private float distanceFromObject;
+	[SerializeField]
+	private Camera _camera;
+    private Vector3 _mousePosition;
+    private Vector3 _direction;
+    private float _distanceFromObject;
 
     private void Update()
     {
-        mousePosition = camera.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-        //Debug.Log(Input.mousePosition);
-        //Rotates toward the mouse
 
-        direction = mousePosition - transform.position;
-        direction.Normalize();
-        Debug.Log("main"+direction);
-        float rot = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;//Mathf.PI;
-        transform.rotation = Quaternion.AngleAxis(rot, Vector3.forward); 
+#region Mouse Control
 
-        //Judge the distance from the object and the mouse
-        distanceFromObject = (Input.mousePosition - camera.WorldToScreenPoint(transform.position)).magnitude;
+//				_mousePosition = _camera.ScreenToWorldPoint (new Vector2 (Input.mousePosition.x, Input.mousePosition.y));
+//				//Debug.Log(Input.mousePosition);
+//				//Rotates toward the mouse
+//
+//				_direction = _mousePosition - transform.position;
+//				_direction.Normalize ();
+//		      
+//				float rot = Mathf.Atan2 (_direction.y, _direction.x) * Mathf.Rad2Deg - 90;//Mathf.PI;
+//				transform.rotation = Quaternion.AngleAxis (rot, Vector3.forward); 
+//
+//				//Judge the distance from the object and the mouse
+//				_distanceFromObject = (Input.mousePosition - _camera.WorldToScreenPoint (transform.position)).magnitude;
+//		      
+//		      
+//
+//				if (Input.GetKey (KeyCode.W)) {
+//						Debug.Log ("Key down [W]");
+//
+//						var targetPosition = new Vector2 (_mousePosition.x, _mousePosition.y);
+//						Debug.Log ("Distance: " + _distanceFromObject);
+//						if (_distanceFromObject > 25) {
+//								transform.position = Vector2.MoveTowards (transform.position, targetPosition, Speed * Time.deltaTime);
+//						} else {
+//								transform.position = transform.position;
+//						}
+//		          
+//				}
+//
+//				if (Input.GetKey (KeyCode.S)) {
+//						Debug.Log ("Key down [S]");
+//						
+//						var targetPosition = new Vector2 (_mousePosition.x, _mousePosition.y);
+//
+//						Debug.Log ("Distance" + _distanceFromObject);
+//
+//						if (_distanceFromObject > 25) {
+//								transform.position = Vector2.MoveTowards (transform.position, targetPosition, -Speed * Time.deltaTime);
+//						} else {
+//								transform.position = transform.position;
+//						}
+//			
+//				}
 
-      
+#endregion
+				
 
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Debug.Log("Key down");
-            direction = new Vector2(mousePosition.x - transform.position.x,mousePosition.y-transform.position.y).normalized;
-            Debug.Log("if"+direction);
-            rigidbody.AddForce(direction * speed * Time.deltaTime);
-           
-            
-        }
+#region Keyboard Control
 
-    }
+		// поворот корабля
+		Quaternion _rotation = transform.rotation;
+		float z = _rotation.eulerAngles.z;
+		z -= Input.GetAxis("Horizontal") * RotationSpeed * Time.deltaTime;
+		_rotation = Quaternion.Euler(0, 0, z);
+		transform.rotation = _rotation;
+		
+		// корабль летит вперед в зависимости от поворота
+		Vector3 _position = transform.position;
+		Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * Speed * Time.deltaTime, 0);
+		_position += _rotation * velocity;
+		transform.position = _position;
 
-    // Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
-            //RaycastHit hit = new RaycastHit();
-            //if (Physics.Raycast(ray, out hit))
-            //{
-            //    Vector3 rot = transform.eulerAngles;
-            //    transform.LookAt(hit.point);
-            //    transform.eulerAngles = new Vector3(rot.x, transform.eulerAngles.y - 90, rot.z);
-            //}
-
-   
-
-    // Use this for initialization
+#endregion
+		
+	}
+	
+	void Awake()
+	{
+		
+	}
+	
 	void Start ()
 	{
-	    
+		_camera = Camera.main;
 	}
 	
 
